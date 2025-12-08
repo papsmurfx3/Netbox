f15adf5adc0a749b6c3ddb8feba61e31eb83a83a
papsmurfx3 <39305344+papsmurfx3@users.noreply.github.com>
2025-12-08 14:37:45 -0500

    Create create_vm.py

---
+++ Scripts/create_vm.py
@@ -0,0 +1,114 @@
+from dcim.models import DeviceRole, Platform
+from django.core.exceptions import ObjectDoesNotExist
+from extras.models import Tag
+from ipam.choices import IPAddressStatusChoices
+from ipam.models import IPAddress, VRF
+from tenancy.models import Tenant
+from virtualization.choices import VirtualMachineStatusChoices
+from virtualization.models import Cluster, VirtualMachine, VMInterface
+from extras.scripts import Script, StringVar, IPAddressWithMaskVar, ObjectVar, MultiObjectVar, ChoiceVar, IntegerVar, TextVar
+
+
+class NewVM(Script):
+
+    class Meta:
+        name = "New VM"
+        description = "Create a new VM"
+
+    # Form fields you’ll see on the “one screen”
+    vm_name = StringVar(label="VM name")
+    dns_name = StringVar(label="DNS name", required=False)
+    vm_tags = MultiObjectVar(model=Tag, label="VM tags", required=False)
+
+    primary_ip4 = IPAddressWithMaskVar(label="IPv4 address")
+    primary_ip6 = IPAddressWithMaskVar(label="IPv6 address", required=False)
+
+    # vrf = ObjectVar(model=VRF, required=False)
+
+    role = ObjectVar(model=DeviceRole, query_params=dict(vm_role=True), required=False)
+    status = ChoiceVar(VirtualMachineStatusChoices,
+                       default=VirtualMachineStatusChoices.STATUS_ACTIVE)
+    cluster = ObjectVar(model=Cluster)
+    tenant = ObjectVar(model=Tenant, required=False)
+    platform = ObjectVar(model=Platform, required=False)
+
+    interface_name = StringVar(default="eth0")
+    mac_address = StringVar(label="MAC address", required=False)
+
+    vcpus = IntegerVar(label="VCPUs", required=False)
+    memory = IntegerVar(label="Memory (MB)", required=False)
+    disk = IntegerVar(label="Disk (GB)", required=False)
+
+    comments = TextVar(label="Comments", required=False)
+
+    def run(self, data, commit):
+
+        # 1) Create the VM
+        vm = VirtualMachine(
+            name=data["vm_name"],
+            role=data["role"],
+            status=data["status"],
+            cluster=data["cluster"],
+            platform=data["platform"],
+            vcpus=data["vcpus"],
+            memory=data["memory"],
+            disk=data["disk"],
+            comments=data["comments"],
+            tenant=data.get("tenant"),
+        )
+        vm.full_clean()
+        vm.save()
+        vm.tags.set(data["vm_tags"])
+
+        # 2) Create a VM interface
+        vminterface = VMInterface(
+            name=data["interface_name"],
+            mac_address=data["mac_address"],
+            virtual_machine=vm,
+        )
+        vminterface.full_clean()
+        vminterface.save()
+
+        # 3) Helper for IP creation/assignment
+        def add_addr(addr, family):
+            if not addr:
+                return
+            if addr.version != family:
+                raise RuntimeError(f"Wrong family for {addr}")
+            try:
+                a = IPAddress.objects.get(
+                    address=addr,
+                    vrf=data.get("vrf"),
+                )
+                a.snapshot()
+                result = "Assigned"
+            except ObjectDoesNotExist:
+                a = IPAddress(
+                    address=addr,
+                    vrf=data.get("vrf"),
+                )
+                result = "Created"
+
+            a.status = IPAddressStatusChoices.STATUS_ACTIVE
+            a.dns_name = data["dns_name"]
+
+            if a.assigned_object:
+                raise RuntimeError(f"Address {addr} is already assigned")
+
+            a.assigned_object = vminterface
+            a.tenant = data.get("tenant")
+            a.full_clean()
+            a.save()
+
+            self.log_info(f"{result} IP address {a.address} {a.vrf or ''}")
+            setattr(vm, f"primary_ip{family}", a)
+
+        vm.snapshot()
+        add_addr(data["primary_ip4"], 4)
+        add_addr(data["primary_ip6"], 6)
+        vm.full_clean()
+        vm.save()
+
+        self.log_success(
+            f"Created VM {vm.name} (/virtualization/virtual-machines/{vm.id}/)"
+        )
